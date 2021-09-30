8f1b710afb0610e364cd7bb82aedb2c88c1f5495
Gilberto Garcia <gilbertogjn@gmail.com>
2021-09-29 20:46:24 -0400

    Fix 1

--- player.js
+++ player.js
@@ -1,9 +1,8 @@
 const video = document.getElementById('playerVideo')
-
+const lista = Array.from(document.getElementsByTagName('li'))
 const botaoPlay = document.querySelector('[w-play]')
 const imgBtPause = '<img src="./imgs/pause.svg" width="35" alt="pause" id="pause">'
 const imgBtPlay = '<img src="./imgs/play.svg" width="35" alt="play" id="play">'
-
 const timer = document.getElementById('timer')
 const timerBarra = document.getElementById('timer-barra')
 const timerDuracao = document.getElementById('timer-duracao')
@@ -39,34 +38,37 @@ function selecionarVideo(id) {
     video.load()
     video.play()
     botaoPlay.innerHTML = imgBtPause
-    elemento.style.backgroundColor = "rgba(0, 0, 0, 0.4)"
-    iconeBars.innerHTML = '<lottie-player id="lottie-bars" src="imgs/soundbars.json" background="transparent" speed="1"  loop autoplay></lottie-player>'
-
-    const lis = Array.from(document.getElementsByTagName('li'))
-
-    lis.forEach(element => {
-        if (element != elemento) {
-            element.style.backgroundColor = "rgba(0, 0, 0, 0)"
-            let id = element.getAttribute('id')
-            document.querySelector('#' + id + ' .musica > .icone-bars').innerHTML = ""
-        }
-    })
 }
 
-/*
 video.addEventListener('play', () => {
-
     const urlSource = source.getAttribute('src')
 
     lista.forEach(element => {
+        const id = element.getAttribute('id')
         const url = element.getAttribute('url')
-        console.log()
-        if (urlSource == url) {
-            element.style.backgroundColor = "rgba(0, 0, 0, 0.4)"
+        const iconeBars = document.querySelector('#' + id + ' .musica > #icone-bars > lottie-player')
+        if (urlSource == url && video.paused == false) {
+            element.className = "musica-selecionada"
+            iconeBars.className = "lottie-bars-show"
+        } else {
+            element.className= "lista-descelecionada"
+            iconeBars.className = "lottie-bars-none"
         }
     })
-}) 
-*/
+})
+
+video.addEventListener('pause', () => {
+    const urlSource = source.getAttribute('src')
+
+    lista.forEach(element => {
+        const id = element.getAttribute('id')
+        const url = element.getAttribute('url')
+        const iconeBars = document.querySelector('#' + id + ' .musica > #icone-bars > lottie-player')
+        if (urlSource == url && video.paused) {
+            iconeBars.className = "lottie-bars-none"
+        }
+    })
+})
 
 video.addEventListener('timeupdate', progressoVideo)
 
@@ -117,7 +119,6 @@ function progressoVideo() {
     timerBarra.style.width = larguraBarra + '%'
 }
 
-
 // Barra de Progresso alterável pelo clique
 barraProgresso.addEventListener('click', (e) => {
     const tempoProgresso = (e.offsetX / barraProgresso.offsetWidth) * video.duration
@@ -128,10 +129,10 @@ function autostart() {
     const source = document.getElementById('source')
     const url = document.getElementById('source').getAttribute('src')
     let videoatual_index = videos_url.indexOf(url)
+    
     if (videoatual_index >= 0 && videoatual_index < videos_url.length ) {
-	source.setAttribute('src',videos_url[videoatual_index+1])
-	video.load()
-	video.play()
+	    source.setAttribute('src',videos_url[videoatual_index+1])
+	    video.load()
+	    video.play()
     }
-
 }
